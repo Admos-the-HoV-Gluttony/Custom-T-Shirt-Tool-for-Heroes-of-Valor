773696d874d12cd60c0675cda5dd97f9e089f6ac
Admos <hovtryhard@gmail.com>
2025-03-07 16:49:35 +0100

    Update Remove UGF.bat

--- data/tools/Remove UGF.bat
+++ data/tools/Remove UGF.bat
@@ -7,27 +7,35 @@ set "targetPath=data\Imported Game Files\HeroesOfValor\Content\Paks\HeroesOfValo
 
 if exist "%targetPath%" (
     echo The directory "%targetPath%" was found and will be deleted.
+    echo.
 ) else (
     echo The directory "%targetPath%" was not found.
+    echo.
     pause
     endlocal
     exit /b
 )
 
 :askConfirmation
+cls
 set "userInput="
 echo Do you want to proceed with the deletion? (y/n)
+echo.
 set /p userInput=
 if /i "%userInput%"=="y" (
     rmdir /s /q "%targetPath%"
     if errorlevel 1 (
+        echo.
         echo Failed to delete the directory.
     ) else (
+        echo.
         echo The directory has been deleted successfully.
     )
 ) else if /i "%userInput%"=="n" (
+    echo.
     echo Deletion cancelled.
 ) else (
+    echo.
     echo Invalid input. Please enter 'y' or 'n'.
     goto askConfirmation
 )
