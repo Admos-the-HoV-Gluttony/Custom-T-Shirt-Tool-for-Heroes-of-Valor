448212acc99dc77e3a1f3006db3076fe9e718df8
Admos <hovtryhard@gmail.com>
2025-03-07 16:43:42 +0100

    Update Packaging.bat

--- data/tools/Packaging.bat
+++ data/tools/Packaging.bat
@@ -8,21 +8,24 @@ echo ============================================
 echo.
 echo 1. Unpackage
 echo 2. Repackage
-echo 3. Quit
+echo 3. Remove Unpackaged Game Files
+echo 4. Quit
 echo.
 echo ============================================
 echo.
-set /p choice=Choose an option (1-3): 
+set /p choice=Choose an option (1-4): 
 
 if "%choice%"=="1" (
     call "data\tools\Unpackage.bat"
 ) else if "%choice%"=="2" (
     call "data\tools\Repackage.bat"
 ) else if "%choice%"=="3" (
+    call "data\tools\Remove UGF.bat"
+) else if "%choice%"=="4" (
     echo Quitting the program.
     exit /b 0
 ) else (
-    echo Invalid option. Please select a number between 1 and 3.
+    echo Invalid option. Please select a number between 1 and 4.
 )
 
 goto menu
