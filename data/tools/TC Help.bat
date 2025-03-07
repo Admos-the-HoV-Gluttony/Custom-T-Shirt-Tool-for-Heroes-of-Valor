e8894dfd1dd4d1d1ea153c1a8bad98b33b92571f
Admos <hovtryhard@gmail.com>
2025-03-07 08:22:51 +0100

    Update TC Help.bat

--- data/tools/TC Help.bat
+++ data/tools/TC Help.bat
@@ -2,12 +2,12 @@
 
 cls
 
-echo Basic Order of operation:
+echo Basic Order of Operation:
 echo.
 echo This assumes you alredy run through the entire setup.
 echo.
 echo 1. Packaging Tools - Unpack 
-echo 2. Inject
+echo 2. Inject - Repeatable before the next step
 echo 3. Packaging Tools - Repackage
 echo 4. Export Modified Game Files
 echo.
