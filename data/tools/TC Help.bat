abbd91f827a1c7d5a1512ea588a530912a9ae27b
Admos <hovtryhard@gmail.com>
2025-03-08 10:14:16 +0100

    Update TC Help.bat

--- data/tools/TC Help.bat
+++ data/tools/TC Help.bat
@@ -9,7 +9,8 @@ echo.
 echo 1. Packaging Tools - Unpack 
 echo 2. Inject - Repeatable before the next step
 echo 3. Packaging Tools - Repackage
-echo 4. Export Modified Game Files
+echo 4. Optional, Recommended: Remove Existing Unpackaged Game Files Before Exporting
+echo 5. Export Modified Game Files
 echo.
 echo Remove Existing Unpackaged Game Files Before Unpacking New Ones!
 echo Please perform this action using the Packaging Tools.
